98b28e952973c7893e8fe304f60ec9dda58888df
chungveng <chungvenghak444@gmail.com>
2014-06-16 10:16:06 +0700

    veng

--- application/models/app_school/ScholarshipDBAccess.php
+++ application/models/app_school/ScholarshipDBAccess.php
@@ -495,9 +495,8 @@ class ScholarshipDBAccess {
         $compusId = isset($params["compusId"]) ? addText($params["compusId"]) : "0";
         $studentId = isset($params["studentId"]) ? addText($params["studentId"]) : "0";
         $scholarship = isset($params["CHOOSE_SCHOLARSHIP"]) ? addText($params["CHOOSE_SCHOLARSHIP"]) : "0";
-        $schoolyear = isset($params["schoolyear"]) ? addText($params["schoolyear"]) : "0";
-
-        $check = self::checkStudentScholarship($studentId, $schoolyear, $compusId, false);
+        $schoolyearId = isset($params["schoolyearId"]) ? addText($params["schoolyearId"]) : "0";
+        $check = self::checkStudentScholarship($studentId, $schoolyearId, $compusId, false);
         $SAVEDATA = array();
 
         if ($check) {
@@ -516,7 +515,7 @@ class ScholarshipDBAccess {
             $SAVEDATA['STUDENT'] = $studentId;
             $SAVEDATA['CAMPUS'] = $compusId;
             $SAVEDATA['SCHOLARSHIP'] = $scholarship;
-            $SAVEDATA['SCHOOLYEAR'] = $schoolyear;
+            $SAVEDATA['SCHOOLYEAR'] = $schoolyearId;
 
             self::dbAccess()->insert('t_student_scholarship', $SAVEDATA);
         }
