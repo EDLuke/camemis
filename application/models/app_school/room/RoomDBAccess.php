f99404ea70fd29fdc796aaf33bda9eef58e104e3
chhevathana <vathanakhmer@gmail.com>
2014-06-19 15:00:39 +0700

    chhe vathana

--- application/models/app_school/room/RoomDBAccess.php
+++ application/models/app_school/room/RoomDBAccess.php
@@ -236,8 +236,8 @@ class RoomDBAccess {
 
     public function allRooms($params, $isJson= true) {
 
-        $start = $params["start"] ? (int) $params["start"] : "0";
-        $limit = $params["limit"] ? (int) $params["limit"] : "50";
+        $start = isset($params["start"]) ? (int) $params["start"] : "0";
+        $limit = isset($params["limit"]) ? (int) $params["limit"] : "50";
 
         $result = $this->getAllRoomsQuery($params);
         $data = array();
