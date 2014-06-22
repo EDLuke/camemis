c2c1cd296e58e39a44a1e3bd4533bd12cecf98fc
chhevathana <vathanakhmer@gmail.com>
2014-06-22 11:25:44 +0700

    chhe vathana

--- application/modules/app_university/controllers/ExportController.php
+++ application/modules/app_university/controllers/ExportController.php
@@ -17,8 +17,6 @@ require_once 'models/export/StudentStatusExportDBAccess.php';//@Visal
 require_once 'models/export/StudentAdvisoryExportDBAccess.php';//@Visal
 require_once 'models/export/StudentTrainingExportDBAccess.php';//@CHHE Vathana
 require_once 'models/export/RoomExportDBAccess.php';//@CHHE Vathana
-//require_once 'models/export/EducationBackgroundStudentExportDBAccess.php';//@CHHE Vathana
-
 
 class ExportController extends Zend_Controller_Action {
 
@@ -37,7 +35,6 @@ class ExportController extends Zend_Controller_Action {
         $this->STUDENT_DISCIPLINE_EXCEL = new StudentDisciplineExportDBAccess($this->_getParam('objectId'));//@veasna
         $this->STUDENT_TRAINING_EXCEL = new StudentTrainingExportDBAccess($this->_getParam('gridId'));//@CHHE Vathana
         $this->ROOM_EXCEL = new RoomExportDBAccess($this->_getParam('objectId'));//@CHHE Vathana
-        //$this->EDUCATION_BACKGROUND_STUDENT_EXCEL = new EducationBackgroundStudentExportDBAccess($this->_getParam('gridId'));//@CHHE Vathana
         
 
         $this->SCHEDULE_EXCEL = new ScheduleExportDBAccess(
@@ -158,10 +155,6 @@ class ExportController extends Zend_Controller_Action {
     {
          
     }
-    public function openeducationbackgroundstudentlistAction()
-    {
-         
-    }
     
     //End...
     
@@ -214,9 +207,6 @@ class ExportController extends Zend_Controller_Action {
                 break;
             case "allRooms":
                 $jsondata = $this->ROOM_EXCEL->allRooms($this->REQUEST->getPost());
-                break;
-            case "educationbackgroundstudent":
-                //$jsondata = $this->EDUCATION_BACKGROUND_STUDENT_EXCEL->educationbackgroundstudent($this->REQUEST->getPost());
                 break;
                 
             //End...
