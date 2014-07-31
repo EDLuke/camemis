b13048c2148a57c2908df7515cd9891deb5b5d21
sor veasna <veasnasor@gmail.com>
2014-07-31 13:02:15 +0700

    config ckeditor

--- public/ckeditor/config.js
+++ public/ckeditor/config.js
@@ -7,4 +7,10 @@ CKEDITOR.editorConfig = function(config) {
     // Define changes to default configuration here. For example:
     // config.language = 'fr';
     // config.uiColor = '#AADC6E';
+	config.filebrowserBrowseUrl = '/public/kcfinder-3.12/browse.php?opener=ckeditor&type=files';
+	config.filebrowserImageBrowseUrl = '/public/kcfinder-3.12/browse.php?opener=ckeditor&type=images';
+	config.filebrowserFlashBrowseUrl = '/public/kcfinder-3.12/browse.php?opener=ckeditor&type=flash';
+	config.filebrowserUploadUrl = '/public/kcfinder-3.12/upload.php?opener=ckeditor&type=files';
+	config.filebrowserImageUploadUrl = '/public/kcfinder-3.12/upload.php?opener=ckeditor&type=images';
+	config.filebrowserFlashUploadUrl = '/public/kcfinder-3.12/upload.php?opener=ckeditor&type=flash';
 };
